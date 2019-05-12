51df576908a2577c3b72f754771e62778a12584c
Husnain Tariq <34330363+hxnain619@users.noreply.github.com>
2019-05-13 01:51:15 +0500

    Add files via upload

---
+++ utils/slugify.js
@@ -0,0 +1,25 @@
+function slugify (incString, separator, preserved) {
+    var p = ['.', '=', '-'];
+    var s = '-';
+    
+    if(typeof preserved != 'undefined') {
+        p = preserved;
+    }
+
+    if(typeof separator != 'undefined') {
+        s = separator;
+    }
+
+    return incString.toLowerCase().
+        replace(/ü/g, 'ue').
+        replace(/ä/g, 'ae').
+        replace(/ö/g, 'oe').
+        replace(/ß/g, 'ss').
+        replace(new RegExp('[' + p.join('') + ']', 'g'), ' ').    //  replace preserved characters with spaces
+        replace(/-{2,}/g, ' ').     //  remove duplicate spaces
+        replace(/^\s\s*/, '').replace(/\s\s*$/, '').    //  trim both sides of string
+        replace(/[^\w\ ]/gi, '').   //  replaces all non-alphanumeric with empty string
+        replace(/[\ ]/gi, s);    //  Convert spaces to dashes
+}
+
+module.exports = slugify;
